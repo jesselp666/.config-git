c8ab509a3d4df64fd07ce34943dd51f605a2c2d5
jesselp666 <farmfiddlin@gmail.com>
2019-08-16 09:00:32 -0400

    pizza rules

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -178,6 +178,7 @@ static Key keys[] = {
      { MODKEY,              XK_comma,  cyclelayout,    {.i = -1  } },
      { MODKEY|ShiftMask,    XK_space,  setlayout,      {0} },
      { MODKEY,              XK_space,  togglefloating, {0} },
+     { MODKEY,              XK_s,      togglesticky,   {0} },
      { MODKEY,              XK_0,      view,           {.ui = ~0 } },
      { MODKEY|ShiftMask,    XK_0,      tag,            {.ui = ~0 } },
      { MODKEY|ControlMask,  XK_comma,  focusmon,       {.i = -1 } },
